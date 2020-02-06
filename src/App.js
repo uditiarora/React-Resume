aa403a3bbf249c3e92060749dcfffacfc22129ee
Uditi <uditiarora@gmail.com>
2020-02-06 18:08:59 +0530

    left panel done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './App.css';
 import Panel from './components/Panel';
 function App() {
+  const name="Hey, I am Uditi Arora.";
+  const subtext="I am a UG student at MNIT, Jaipur.";
+  const jobStatus="";
+  const twitterURL="";
+  const instaURL="";
+  const githubURL="";
+  const linkedinURL="";
   return (
     
         <div>
-          <Panel />
+          <Panel 
+            name = {name}
+            subtext = {subtext}
+            jobStatus = {jobStatus}
+            twitterURL = {twitterURL}
+            instaURL = {instaURL}
+            githubURL = {githubURL}
+            linkedinURL = {linkedinURL}
+          />
         </div>
       
   );
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import '../css/panel.css';
-function Panel(){
+function Panel(props){
     return(
         <div className="left">
             <div className="bg-img">
-
+                <div className="left-inner">
+                    <div className="info">
+                        <h2 className="name">{ props.name}</h2>
+                        <div className="subtext">{ props.subtext}</div>
+                        <div className="jobStatus">{ props.jobStatus}</div>
+                        <div className="social-links">
+                            <a href={ props.twitterURL} target="_blank"><i className="fa fa-twitter"></i></a>
+                            <a href={ props.instaURL} target="_blank"><i className="fa fa-instagram"></i></a>
+                            <a href={ props.githubURL} target="_blank"><i className="fa fa-github"></i></a>
+                            <a href={ props.linkedinURL}  target="_blank"><i className="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            {/* <img src={require('../img/me.jpg')} height="100%" width="100%"/> */}
         </div>
     );
 }
