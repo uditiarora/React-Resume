e99410d449beeee342cb35d068e952fc66884b57
Uditi <uditiarora@gmail.com>
2020-02-08 00:22:46 +0530

    finishing up with the design

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -7,6 +7,9 @@ function Resume(){
     return(
         <div className="right">
             {Object.keys(data).map((obj, i) => <div><ResumeTile key = {i} obj = {obj} tile_data = {data[obj]}/></div>)}
+            <br />
+            <br />
+            <a className="resume" href = "https://drive.google.com/open?id=1srvf3R2JIBtxNJGbc5lWPZ7mQPkrGKgL" target="_blank" download><div className="resume-link">Grab a PDF of my full resume</div></a>
         </div>
     );
 }
