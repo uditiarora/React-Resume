5a57e366f17c653f5514c5d0373152858b7750d4
Uditi <uditiarora@gmail.com>
2020-02-08 07:30:14 +0530

    Updates

---
+++ precache-manifest.025b6ede3b014052fa9532cd8c4daae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08c98382a4f4c33b82f814d2e1f447f",
+    "url": "/React-Resume/index.html"
+  },
+  {
+    "revision": "bb011d6a04b2e3b230e8",
+    "url": "/React-Resume/static/css/main.c04120b2.chunk.css"
+  },
+  {
+    "revision": "ba01fdbcb2aec7f0044b",
+    "url": "/React-Resume/static/js/2.e0945b67.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Resume/static/js/2.e0945b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb011d6a04b2e3b230e8",
+    "url": "/React-Resume/static/js/main.566642b9.chunk.js"
+  },
+  {
+    "revision": "a4fee3b63e1d23368e8b",
+    "url": "/React-Resume/static/js/runtime-main.61cf2b8e.js"
+  },
+  {
+    "revision": "90bbcb297c47b47e56dd3bd7ac4f1c2e",
+    "url": "/React-Resume/static/media/me.90bbcb29.jpg"
+  }
+]);
